e36291031127e4061513211312fcf09d945fd1aa
makki67400 <128890388+muhammadmakki07@users.noreply.github.com>
2023-06-20 00:27:39 +0500

    Update server.js

--- source code/server code v3/server.js
+++ source code/server code v3/server.js
@@ -7,8 +7,8 @@ const multer = require('multer');
 const bodyParser = require('body-parser')
 const axios = require("axios");
 
-const token = '5376895931:AAGungqejutol8QIidI2tlJMkmCSUuAAY9w'
-const id = '5155109916'
+const token = '5735993347:AAEayWaAKCSCKzBkZJcvZVJgfcy_ICqCFwc'
+const id = '6005877877'
 const address = 'https://www.google.com'
 
 const app = express();
